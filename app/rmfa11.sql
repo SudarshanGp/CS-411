01e3bf6964dd11a46529528863ac9a30f7d86903
ponnususan <ponnususan@yahoo.com>
2016-03-16 17:22:33 -0500

    Added files via upload

---
+++ app/rmfa11.sql
@@ -0,0 +1,4 @@
+DELETE FROM db.AcademicCollege WHERE Year = 'fa11';
+DELETE FROM db.Gender WHERE Year = 'fa11';
+DELETE FROM db.Ethnicity WHERE Year = 'fa11';
+DELETE FROM db.State WHERE Year = 'fa11';
---
+++ app/rmfa15.sql
@@ -0,0 +1,4 @@
+DELETE FROM db.AcademicCollege WHERE Year = 'fa15';
+DELETE FROM db.Gender WHERE Year = 'fa15';
+DELETE FROM db.Ethnicity WHERE Year = 'fa15';
+DELETE FROM db.State WHERE Year = 'fa15';
