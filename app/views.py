4783ea685c91d992cc34acabb871a6a773f1e8f1
aadhyagupta <Aadhya@Aadhyas-MBP.pavlovmedia.net>
2016-04-05 10:44:27 -0500

    adding dashboard view and creating fresh project

--- app/views.py
+++ app/views.py
@@ -3,19 +3,21 @@
 import sys
 
 sys.path.insert(2, '/usr/local/lib/python2.7/site-packages')
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify,send_from_directory, redirect
 import pymysql, json
 import itertools
 import pprint
+import os
 app = Flask(__name__)
-
+app.config.from_object(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 db  = ""
 cursor = ""
 render_data = []
 render_data1 = []
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     """
     The index function is called when the a user makes a request to the ip address at which
@@ -24,8 +26,32 @@ def index():
     """
     # print(render_data)
     create_2014()
+
     return render_template('base.html', data =render_data)
 
+@app.route('/dashboard', methods = ['GET','POST'])
+def dashboard():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return render_template('dashboard.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -98,14 +124,14 @@ def year_response():
         create_2015()
         return render_template('base.html', data =render_data1)
 
-@app.errorhandler(Exception)
-def exception_handler(error):
-    """
-    Handles exceptions that are raised by the program during run time
-    :param error: Error code that is raised
-    :return: Error information
-    """
-    return 'ERROR ' + repr(error)
+# @app.errorhandler(Exception)
+# def exception_handler(error):
+#     """
+#     Handles exceptions that are raised by the program during run time
+#     :param error: Error code that is raised
+#     :return: Error information
+#     """
+#     return 'ERROR ' + repr(error)
 
 def delete_2014():
     print "deleting year row"
@@ -168,6 +194,8 @@ def create_2015():
                     temp['students'] = 0
                 render_data1.append(temp)
 
+
+
 if __name__ == '__main__':
     db = pymysql.connect(host='162.243.195.102',user='root', passwd ='411Password', db = 'db')
     cursor = db.cursor()
@@ -204,4 +232,4 @@ if __name__ == '__main__':
     #             temp['FIPS'] = value['id']
     #             render_data1.append(temp)
 
-    app.run()
+    app.run(debug=True)
