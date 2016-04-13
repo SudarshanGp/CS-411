300a88aa938158aaa5718202a93310540159f78a
ponnususan <ponnususan@yahoo.com>
2016-04-12 22:36:30 -0500

    Added files via upload
    bug fixes

--- app/NewFileParser2.py
+++ app/NewFileParser2.py
@@ -6,10 +6,26 @@ import xlrd #for reading .xlsx files
 import csv #for readin .csv files
 import sys  #getting sys arguments
 import os #for getting cwd
+import pymysql
 
 db_name = 'Demo.db'
 CurrentDir= os.getcwd()+"/static/res/"
+cursor = ""
 # /Users/Aadhya/GitHub
+def locInDict(dict, dept, majval):
+	count = 0
+	locList = 0
+	for key in dict:
+		if key != dept:
+			count += len(key) 
+		else:
+			for val in dict[key]:
+				if val==majval:
+					return dict[key][majval], locList
+				count += 1
+				locList += 1
+		locList = 0
+	return count, locList
 
 def buildOutputSQL(file_name, db_name):
    f = open(file_name, "wb")
@@ -20,7 +36,6 @@ def buildOutputSQL(file_name, db_name):
     Year VARCHAR(8) not null,
     Department VARCHAR(128) not null,
     Major VARCHAR(128) not null,
-    Total int not null,
     primary key (ID)
     );\n''')
 
@@ -55,82 +70,79 @@ def buildOutputSQL(file_name, db_name):
     );\n''')
    f.close
 
-def FilePar(fileloc):
+def FilePar(fileloc, datb):
    
-    Gender=[]
-    Ethnicity=[]
-    Residency = []
+    Gender={}
+    Ethnicity={}
+    Residency = {}
     Department={}
     Departmentlist=[]
     wb=xlrd.open_workbook(fileloc)
     current_sheet=wb.sheet_by_index(0)
-  
+
+    retCounter=0
     dep = 0
-    counter=0 # for ID
+    counter = 0
+    db = pymysql.connect(host='162.243.195.102',user='root', passwd ='411Password', db = 'db')
+    cursor = db.cursor()
+    check="SHOW DATABASES LIKE '"+datb+"';"
+    cursor.execute(check) # for ID
+    exist = cursor.fetchall()
+    if len(exist) > 0:
+    	check="SELECT COUNT(*) FROM "+datb+".id;"
+    	cursor.execute(check) # for ID
+    	counter = cursor.fetchall()
+    	counter = int(counter[0][0])
+    	retCounter = counter
     counterlist=[]
     
     for i in range(1, current_sheet.nrows):
-        
         if(i>11):
             #print 'a'+current_sheet.cell(i,2).value+'a'
             #it was a double space for some reason....
             
-            if(current_sheet.cell(i,2).value=='  ' and current_sheet.cell(i,3).value=='  ' and 
+			if(current_sheet.cell(i,2).value=='  ' and current_sheet.cell(i,3).value=='  ' and 
                 current_sheet.cell(i,4).value=='  ' and current_sheet.cell(i,5).value!='  ' and 
-                current_sheet.cell(i,6).value=='  ' and current_sheet.cell(i,7).value=='  '):
-                thisdep = '"'+current_sheet.cell(i,5).value
-                thisdep=thisdep[0:len(thisdep)-1]
-                thisdep+='"'
-                Departmentlist.append(thisdep)
-                dep += 1
-                Department[Departmentlist[dep-1]]=[]
-                
-                continue
-        
-            if(current_sheet.cell(i,5).value!=''):
-            	maj='"'+current_sheet.cell(i,5).value
-                maj= maj[0:len(maj)-1]
-                maj+='"'
-                if (maj not in Department[Departmentlist[dep-1]]):
-                    Department[Departmentlist[dep-1]].append(maj)
-                    counter += 1
-                if counter in counterlist:
-                    #find same counter then add the points
-                    for j in range(0,len(Gender)):
-                        #all are the same length
-                        if(Gender[j][0]==counter):
-                            Gender[j][1]+=current_sheet.cell(i,9).value
-                            Gender[j][2]+=current_sheet.cell(i,10).value
-                            Gender[j][3]+=current_sheet.cell(i,11).value
-                    
-                        if(Ethnicity[j][0]==counter):
-                            #could change this too a for loop.
-                            Ethnicity[j][1]+=current_sheet.cell(i,12).value
-                            Ethnicity[j][2]+=current_sheet.cell(i,13).value
-                            Ethnicity[j][3]+=current_sheet.cell(i,14).value
-                            Ethnicity[j][4]+=current_sheet.cell(i,15).value
-                            Ethnicity[j][5]+=current_sheet.cell(i,16).value
-                            Ethnicity[j][6]+=current_sheet.cell(i,17).value
-                            Ethnicity[j][7]+=current_sheet.cell(i,18).value
-                            Ethnicity[j][8]+=current_sheet.cell(i,19).value
-                            Ethnicity[j][9]+=current_sheet.cell(i,20).value
-                            
-                        if(Residency[j][0]==counter):
-                            Residency[j][1]+=current_sheet.cell(i,19).value
-                            Residency[j][2]+=current_sheet.cell(i,20).value
-                            
-                else:
-                    #not the same counter so just add to the list
-                    counterlist.append(counter)
-                
-                    Gender.append([counter,current_sheet.cell(i,9).value, current_sheet.cell(i,10).value, current_sheet.cell(i,11).value])
-                
-                    Ethnicity.append([counter,current_sheet.cell(i,12).value,current_sheet.cell(i,13).value,current_sheet.cell(i,14).value, 
-                                    current_sheet.cell(i,15).value, current_sheet.cell(i,16).value, current_sheet.cell(i,17).value, 
-                                    current_sheet.cell(i,18).value, current_sheet.cell(i,19).value, current_sheet.cell(i,20).value])
-                    Residency.append([counter,current_sheet.cell(i,19).value,current_sheet.cell(i,20).value])
-   
-    return Department,Gender,Ethnicity,Residency
+                current_sheet.cell(i,9).value=='  ' and current_sheet.cell(i,10).value=='  '):
+				thisdep = '"'+current_sheet.cell(i,5).value
+				thisdep=thisdep[0:len(thisdep)-1]
+				thisdep+='"'
+				Departmentlist.append(thisdep)
+				dep += 1
+				Department[Departmentlist[dep-1]]={}
+				continue
+			elif(current_sheet.cell(i,4).value!='  ' and current_sheet.cell(i,5).value!='  ' and 
+				type(current_sheet.cell(i,9).value) is float):
+				maj='"'+current_sheet.cell(i,5).value
+				maj= maj[0:len(maj)-1]
+				maj+='"'
+				if (maj not in Department[Departmentlist[dep-1]].keys()):
+					Department[Departmentlist[dep-1]][maj]=counter
+					counter += 1
+				tempc = Department[Departmentlist[dep-1]][maj]
+				if(tempc in Gender.keys()):
+					j=tempc
+					Gender[j][0]+=current_sheet.cell(i,9).value
+					Gender[j][1]+=current_sheet.cell(i,10).value
+					Gender[j][2]+=current_sheet.cell(i,11).value
+
+					Ethnicity[j][0]+=current_sheet.cell(i,12).value
+					Ethnicity[j][1]+=current_sheet.cell(i,13).value
+					Ethnicity[j][2]+=current_sheet.cell(i,14).value
+					Ethnicity[j][3]+=current_sheet.cell(i,15).value
+					Ethnicity[j][4]+=current_sheet.cell(i,16).value
+					Ethnicity[j][5]+=current_sheet.cell(i,17).value
+					Ethnicity[j][6]+=current_sheet.cell(i,18).value
+					Ethnicity[j][7]+=current_sheet.cell(i,19).value
+					Ethnicity[j][8]+=current_sheet.cell(i,20).value
+
+					Residency[j][0]+=current_sheet.cell(i,23).value
+					Residency[j][1]+=current_sheet.cell(i,24).value
+				else:
+					Gender[tempc]=[0,0,0]
+					Ethnicity[tempc]=[0,0,0,0,0,0,0,0,0]
+					Residency[tempc]=[0,0]
+    return Department,Gender,Ethnicity,Residency, retCounter
 
 def writeInsert(file_name, db_name, list, insType, first):
     if first == True:
@@ -138,16 +150,16 @@ def writeInsert(file_name, db_name, list, insType, first):
     else:
         f=open(file_name,"a")
 
-    for i in range(len(list)):
+    for i in list:
         f.write("INSERT IGNORE INTO ")
         f.write(db_name)
         f.write(".")
         f.write(insType)
         f.write(" VALUES(")
         f.write("'")
-        f.write(str(list[i][0]))
+        f.write(str(i[0]))
         f.write("',")
-        temp = list[i][1:]
+        temp = i[1:]
         f.write(",".join(str(j) for j in temp))
         f.write(");\n")
     f.close()
@@ -169,37 +181,65 @@ def writeDel(file_name, db_name, insType, first):
     f.close()
 
 def main():
-    dbName = "db"
+    dbName = "db3"
 
     fileloc=CurrentDir+str(sys.argv[1])
     file_name = str(sys.argv[1])[:-4]+".sql"
     # rm_file = "rm"+file_name
-    # createNew = ""
-    # if(len(sys.argv) == 3):
-    #     createNew = str(sys.argv[2])
+    createNew = ""
+    if(len(sys.argv) == 3):
+        createNew = str(sys.argv[2])
     
     first = True;
 
-    # if(createNew is 'n'):
-    #     buildOutputSQL(file_name, dbName)
-    #     first = False;
+    if(createNew is 'n'):
+        buildOutputSQL(file_name, dbName)
+        first = False;
 
-    Department, Gender, Ethnicity, Residency=FilePar(fileloc)
+    Department, Gender, Ethnicity, Residency, count = FilePar(fileloc, dbName)
     ids = []
     gen = []
     eth = []
     res = []
-    count=0
 
     for key in Department:
         for val in Department[key]:
-            temp=[]
-            temp.append(str(count))
-            temp.append('"'+str(sys.argv[1])[:-4]+'"')
-            temp.append(key)
-            temp.append(val)
-            ids.append(temp)
-            count += 1
+        	temp=[]
+        	temp.append(str(count))
+        	temp.append('"'+str(sys.argv[1])[:-4]+'"')
+        	temp.append(key)
+        	temp.append(val)
+        	ids.append(temp)
+        	count += 1
+
+    for key in Gender:
+        temp=[]
+        temp.append(key)
+        temp.append(Gender[key][0])
+        temp.append(Gender[key][1])
+        temp.append(Gender[key][2])
+        gen.append(temp)
+   
+    for key in Ethnicity:
+    	temp=[]
+    	temp.append(key)
+    	temp.append(Ethnicity[key][0])
+    	temp.append(Ethnicity[key][1])
+    	temp.append(Ethnicity[key][2])
+    	temp.append(Ethnicity[key][3])
+    	temp.append(Ethnicity[key][4])
+    	temp.append(Ethnicity[key][5])
+    	temp.append(Ethnicity[key][6])
+    	temp.append(Ethnicity[key][7])
+    	temp.append(Ethnicity[key][8])
+    	eth.append(temp)
+    
+    for key in Residency:
+    	temp=[]
+    	temp.append(key)
+    	temp.append(Residency[key][0])
+    	temp.append(Residency[key][1])
+    	res.append(temp)
     # AcademicList=AcademicCollege(fileloc) 
     # GenderList=Gender(fileloc)
     # EthnicityList=Ethnicity(fileloc)
@@ -207,11 +247,11 @@ def main():
     writeInsert(file_name, dbName, ids, "id", first)
     # writeDel(rm_file, dbName, "id", first)
     first = False;
-    writeInsert(file_name, dbName, Gender, "Gender", first)
+    writeInsert(file_name, dbName, gen, "Gender", first)
     # writeDel(rm_file, dbName, "Gender", first)
-    writeInsert(file_name, dbName, Ethnicity, "Ethnicity", first)
+    writeInsert(file_name, dbName, eth, "Ethnicity", first)
     # writeDel(rm_file, dbName, "Ethnicity", first)
-    writeInsert(file_name, dbName, Residency, "Residency", first)
+    writeInsert(file_name, dbName, res, "Residency", first)
     # writeDel(rm_file, dbName, "State", first)
 
 
